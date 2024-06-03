fcb061d7c9057a007cfadc4d70b490a36a1c84f4
Remi-godji <146717466+Remi-Godji@users.noreply.github.com>
2024-06-03 17:00:04 +0200

    job 05 finish

--- Jour01/Job05/index.php
+++ Jour01/Job05/index.php
@@ -4,20 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Affichage des nombres</title>
-    <style>
-        .special {
-            font-weight: bold;
-            text-decoration: underline;
-        }
-    </style>
 </head>
 <body>
     <?php
+    // Tableau des nombres à exclure
+    $exclusions = [26, 37, 88, 1111];
+
     // Boucle pour afficher les nombres de 0 à 1337
     for ($i = 0; $i <= 1337; $i++) {
-        if ($i == 42) {
-            echo "<span class='special'>$i</span><br>";
-        } else {
+        // Vérifie si le nombre est dans le tableau des exclusions
+        if (!in_array($i, $exclusions)) {
             echo "$i<br>";
         }
     }
