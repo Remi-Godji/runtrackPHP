fb245817f30fb09497c93190880c6adf80f2de15
Remi-godji <146717466+Remi-Godji@users.noreply.github.com>
2024-06-06 10:46:51 +0200

    index.php

---
+++ Jour02/Job05/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire de Connexion</title>
+</head>
+<body>
+
+<h2>Formulaire de Connexion</h2>
+
+<form action="login.php" method="post">
+    <label for="username">Nom d'utilisateur:</label>
+    <input type="text" id="username" name="username"><br><br>
+    <label for="password">Mot de passe:</label>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Connexion">
+</form>
+
+</body>
+</html>
