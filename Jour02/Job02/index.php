09fbeb33a150eb40930270ac3f2bdb1a5f8e1537
Remi-godji <146717466+Remi-Godji@users.noreply.github.com>
2024-06-06 10:07:02 +0200

    index.php

---
+++ Jour02/Job02/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Tableau des Arguments $_GET</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Tableau des Arguments $_GET</h2>
+
+<?php include 'display_get.php'; ?>
+
+</body>
+</html>
