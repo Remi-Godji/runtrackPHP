9729740d4ba2a9f62280cbda0bc4fbc4c892824e
Remi-godji <146717466+Remi-Godji@users.noreply.github.com>
2024-06-06 10:54:11 +0200

    Job06.php

---
+++ Jour02/Job06/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Vérification de Nombre</title>
+</head>
+<body>
+
+<h2>Vérification de Nombre</h2>
+
+<form action="vérification.php" method="get">
+    <label for="nombre">Entrez un nombre :</label>
+    <input type="text" id="nombre" name="nombre">
+    <input type="submit" value="Vérifier">
+</form>
+
+</body>
+</html>
